d792f943a74875aa626c801a63ade4575ab0dc44
Jonni <enterthesource@me.com>
2022-05-15 21:45:33 +0200

    Done.

---
+++ UIKitToSwiftUI/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  UIKitToSwiftUI
+//
+//  Created by Jonni Akesson on 2022-05-15.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
--- UIKitToSwiftUI/ViewController.swift
+++ UIKitToSwiftUI/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        extractView()
     }
 
-
+    func extractView() {
+        let hostView = UIHostingController(rootView: SwiftUIView())
+        hostView.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(hostView.view)
+        
+        let constraints = [
+            hostView.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hostView.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            hostView.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hostView.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            hostView.view.widthAnchor.constraint(equalTo: view.widthAnchor),
+            hostView.view.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ]
+        self.view.addConstraints(constraints)
+    }
 }
 
